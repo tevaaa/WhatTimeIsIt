48a65b916843bdb2c8ce61b197a6d170a7c4fc51
tevaaa <andretevaa@gmail.com>
2022-05-08 19:32:27 +0200

    first step

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,24 @@
-const input = document.getElementById('input')      // get input from user
-input.addEventListener('keyup',function(event) {
-    if(event.code == 'Enter')
+var datalist = document.getElementById('select');
+
+// GET ALL CITY LIST -----
+fetch('../json/timezone.json')
+  .then(response => response.json())
+  .then(data => readTimeZone(data))
+  .catch(error => console.log(error));
+
+function readTimeZone (data)                               
+{
+    for(i in data)
     {
-        event.preventDefault();
-        console.log(input.value)
+        datalist.innerHTML += '<option value= '+ data[i] + '>'+ data[i] +'</option>';
     }
-}) 
+}
+
+// -----------------------
+
+document.getElementById("search").addEventListener("click", function(){
+    value = input.value;
+    console.log(value);
+});
+
+
